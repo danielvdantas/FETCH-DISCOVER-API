0fad0f2b2b7da177e94700985d3fa99c9a12a8e6
danielvdantas <danielvdantas@hotmail.com>
2022-08-12 10:06:37 -0300

    Código

---
+++ main.js
@@ -0,0 +1,13 @@
+const url = "http://localhost:5500/api"
+
+function getUsers() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent =
+        JSON.stringify(data))
+    .catch(error => console.error(error))
+}
+
+func
+
+getUsers()
